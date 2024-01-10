483fc32296e49122c0d120ce249973dcf0ffe89e
swoyamshk <swoyam301@gmail.com>
2024-01-11 00:09:06 +0545

    hero section added

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,6 +1,68 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
+
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
+  return (
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div>
+            <Image src='/folded-map.svg' alt="map" height={28} width={28} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">
+              {title}
+            </h4>
+            <p className="regular-14 text-white ">
+              {subtitle}
+            </p>
+          </div>    
+        </div>
+        <div className="flexCenter gap-6">
+            <span className="flex -space-x-4 overflow-hidden">
+              {PEOPLE_URL.map((url) => (
+                <Image src={url} key={url} alt="person" width={52} height={52} />
+              ))}
+
+            </span>
+            <p className="bold-16t md:bold-20 text-white">{peopleJoined}</p>
+
+          </div>
+      </div>
+    </div>
+  )
+}
+
+
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite backgroundImage='bg-bg-img-1' title="Kathmandu Camp" subtitle="Kathmandu,Nepal" peopleJoined="50+ joined" />
+        <CampSite backgroundImage='bg-bg-img-2' title="Mountain View" subtitle="Somewhere in the Mountains" peopleJoined="50+ joined" />
+      </div>
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 px:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="text-white regular-24 md:regular-32 2xl:regular-64 capitalize">
+            <strong>Feeling Lost </strong>
+          And Not Knowing The Way?
+          </h2>
+          <p className="regular-14 text-white mt-5 xl:regular-16">
+          Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+          </p>
+          <Image src="/quote.svg" alt="quote" width={186} height={219} className="camp-quote"/>
+        </div>
+      </div>
+    </section>
+
   )
 }
 
