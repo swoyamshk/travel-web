60c7b82669bc2fea904560a2291142fbf72655ca
swoyamshk <swoyam301@gmail.com>
2024-01-11 15:39:21 +0545

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
         <main className='relative overflow-hidden'>
         {children}
         </main>
-        
         <Footer />
       </body>
     </html>
